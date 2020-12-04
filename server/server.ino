744a88d09163093867443e370975a374eb006876
ManuelFSousa <manuelsousa2001@gmail.com>
2020-12-04 01:17:23 +0000

    Problemas de Compilação Corrigidos

--- server/server.ino
+++ server/server.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>
 
-const char* ssid = "definir"
-const char* password = "definir"
+const char* ssid = "definir";
+const char* password = "definir";
 
 IPAddress ip(192, 168, 13, 80);            // IP address of the server
 IPAddress gateway(192,168,0,1);           // gateway of your network
@@ -12,15 +12,15 @@ void setup(){
 
     //-----------------------------------Ligação ao WIFI-----------------------------------
     WiFi.config(ip, gateway, subnet);
-    WiFi.begin(ssid, pass); 
+    WiFi.begin(ssid, password); 
     Serial.println("**************Servidor de câmaras no ar da bandAlegre**************");
-    Serial.println("\nA conectar ao WIFI %s", ssid);
+    Serial.println("\nA conectar ao WIFI "); Serial.print(ssid);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
     Serial.println("\nConexão bem sucedida!");
-    Serial.println("O IP do servidor é: %s", Wifi.localIP());
+    Serial.println("O IP do servidor é: "); Serial.print(WiFi.localIP());
     //---------------------------------------------------------------------------------------
 
     //--------------------------------Setup dos Pinos a usar---------------------------------
@@ -38,23 +38,23 @@ void setup(){
 void loop(){
     int estado1 = digitalRead(D1), estado2 = digitalRead(D2), estado3 = digitalRead(D3), estado4 = digitalRead(D4);
     if(digitalRead(D1) != estado1){
-        estado1 = digitalRead(D1)
-        Serial.println("Pino1: %d", digitalRead(D1));
+        estado1 = digitalRead(D1);
+        Serial.println("Pino1: "); Serial.print(digitalRead(D1));
         digitalWrite(D5, digitalRead(D1));
     }
     else if(digitalRead(D2) != estado1){
-        estado2 = digitalRead(D2)
-        Serial.println("Pino2: %d", digitalRead(D2));
+        estado2 = digitalRead(D2);
+        Serial.println("Pino2: "); Serial.print(digitalRead(D2));
         digitalWrite(D6, digitalRead(D2));
     }
     else if(digitalRead(D3) != estado1){
-        estado3 = digitalRead(D3)
-        Serial.println("Pino3: %d", digitalRead(D3));
+        estado3 = digitalRead(D3);
+        Serial.println("Pino3: "); Serial.print(digitalRead(D3));
         digitalWrite(D7, digitalRead(D3));
     }
     else if(digitalRead(D4) != estado1){
-        estado4 = digitalRead(D4)
-        Serial.println("Pino4: %d", digitalRead(D4));
+        estado4 = digitalRead(D4);
+        Serial.println("Pino4: "); Serial.print(digitalRead(D4));
         digitalWrite(D8, digitalRead(D4));
     }
 }
