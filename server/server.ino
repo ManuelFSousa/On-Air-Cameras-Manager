f8032d464d7437041402ce52547674327c4e5194
ManuelFSousa <manuelsousa2001@gmail.com>
2020-12-05 19:57:30 +0000

    Corrigido Problema de Boot. Criação Configuração

--- server/server.ino
+++ server/server.ino
@@ -4,9 +4,19 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <ESP8266WiFi.h>
+#include "WiFi_Config.h"
 
-const char* ssid = "definir";
-const char* password = "definir";
+#define B1 D0
+#define B2 D1
+#define B3 D2
+#define B4 D4               //Evitar usar o pin D3, pois se puxado a LOW, o NodeMCU não vai conseguir inicializar. (Como alternativa, pode ser usado para o botão, estando este premido)
+#define L1 D5
+#define L2 D6
+#define L3 D7
+#define L4 D8
+
+const char* ssid = ssidConfig;
+const char* password = passConfig;
 
 IPAddress ip(192, 168, 1, 13);
 IPAddress gateway(192,168,1,1);
@@ -31,47 +41,47 @@ void setup(){
     //---------------------------------------------------------------------------------------
     server.begin();
     //--------------------------------Setup dos Pinos a usar---------------------------------
-    pinMode(D1, INPUT); //Pin Botão 1
-    pinMode(D2, INPUT); //Pin Botão 2
-    pinMode(D3, INPUT); //Pin Botão 3
-    pinMode(D4, INPUT); //Pin Botão 4
-    pinMode(D5, OUTPUT); //Pin do LED 1
-    pinMode(D6, OUTPUT); //Pin do LED 2
-    pinMode(D7, OUTPUT); //Pin do LED 3
-    pinMode(D8, OUTPUT); //Pin do LED 4
+    pinMode(B1, INPUT); //Pin Botão 1
+    pinMode(B2, INPUT); //Pin Botão 2
+    pinMode(B3, INPUT); //Pin Botão 3
+    pinMode(B4, INPUT); //Pin Botão 4
+    pinMode(L1, OUTPUT); //Pin do LED 1
+    pinMode(L2, OUTPUT); //Pin do LED 2
+    pinMode(L3, OUTPUT); //Pin do LED 3
+    pinMode(L4, OUTPUT); //Pin do LED 4
     //---------------------------------------------------------------------------------------
 }
 
 void loop(){
-    int estado1 = digitalRead(D1), estado2 = digitalRead(D2), estado3 = digitalRead(D3), estado4 = digitalRead(D4);
+    int estado1 = digitalRead(B1), estado2 = digitalRead(B2), estado3 = digitalRead(B3), estado4 = digitalRead(B4);
     WiFiClient client = server.available();
     
     while(1){
-        if(digitalRead(D1) != estado1){
-            estado1 = digitalRead(D1);
-            Serial.print("\nPino1: "); Serial.print(digitalRead(D1));
-            digitalWrite(D5, digitalRead(D1));
-            client.printf("Led1 = %d", digitalRead(D1));
+        if(digitalRead(B1) != estado1){
+            estado1 = digitalRead(B1);
+            Serial.print("\nPino1: "); Serial.print(digitalRead(B1));
+            digitalWrite(L1, digitalRead(B1));
+            server.printf("Led1 = %d", digitalRead(B1));
         }
-        else if(digitalRead(D2) != estado2){
-            estado2 = digitalRead(D2);
-            Serial.print("\nPino2: "); Serial.print(digitalRead(D2));
-            digitalWrite(D6, digitalRead(D2));
-            client.printf("Led2 = %d", digitalRead(D2));
+        else if(digitalRead(B2) != estado2){
+            estado2 = digitalRead(B2);
+            Serial.print("\nPino2: "); Serial.print(digitalRead(B2));
+            digitalWrite(L2, digitalRead(B2));
+            server.printf("Led2 = %d", digitalRead(D2));
         }
-        else if(digitalRead(D3) != estado3){
-            estado3 = digitalRead(D3);
-            Serial.print("\nPino3: "); Serial.print(digitalRead(D3));
-            digitalWrite(D7, digitalRead(D3));
-            client.printf("Led3 = %d", digitalRead(D3));
+        else if(digitalRead(B3) != estado3){
+            estado3 = digitalRead(B3);
+            Serial.print("\nPino3: "); Serial.print(digitalRead(B3));
+            digitalWrite(L3, digitalRead(B3));
+            server.printf("Led3 = %d", digitalRead(D3));
         }
-        else if(digitalRead(D4) != estado4){
-            estado4 = digitalRead(D4);
-            Serial.print("\nPino4: "); Serial.print(digitalRead(D4));
-            digitalWrite(D8, digitalRead(D4));
-            client.printf("Led4 = %d", digitalRead(D4));
+        else if(digitalRead(B4) != estado4){
+            estado4 = digitalRead(B4);
+            Serial.print("\nPino4: "); Serial.print(digitalRead(B4));
+            digitalWrite(L4, digitalRead(B4));
+            server.printf("Led4 = %d", digitalRead(B4));
         }
-        client.flush();
+        server.flush();
         delay(500);
     }    
 }
